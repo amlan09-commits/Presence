970ce4c72f6030f3e23861e29499c4bb10b08155
amlan09-commits <github.amlan09@gmail.com>
2026-02-19 00:07:34 +0530

    Initialized server side with express and added a user model

---
+++ server/src/index.js
@@ -0,0 +1,7 @@
+import dotenv from 'dotenv'
+import app from './app.js'
+dotenv.config({path: './.env'})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server started successfully on port ${process.env.PORT}`)
+})
