942203e2e553a3fa44e3de9773b35487925098fd
amlan09-commits <github.amlan09@gmail.com>
2026-02-20 02:19:09 +0530

    Added routing and created the homepage and also added tailwindcss

--- client/presence/src/App.tsx
+++ client/presence/src/App.tsx
@@ -1,34 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route } from "react-router-dom"
+import Homepage from "./components/Homepage"
+import Loginpage from "./components/Loginpage"
+import Registerpage from "./components/Registerpage"
+import Dashboard from "./components/Dashboard"
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Routes>
+      <Route path='/' element={<Homepage/>}/>
+      <Route path='/login' element={<Loginpage/>}/>
+      <Route path='/register' element={<Registerpage/>}/>
+      <Route path='/dashboard' element={<Dashboard/>}/>
+    </Routes>
   )
 }
 
---
+++ client/presence/src/components/Homepage.tsx
@@ -0,0 +1,36 @@
+import { Link } from "react-router-dom"
+
+const Homepage = () => {
+  return (
+    <div className="relative min-h-screen flex flex-col items-center justify-center overflow-hidden bg-[#0f172a] text-white">
+
+      <div className="absolute top-10 left-10 w-96 h-96 bg-purple-600 rounded-full blur-3xl opacity-30"></div>
+
+      <div className="absolute bottom-10 right-10 w-96 h-96 bg-fuchsia-500 rounded-full blur-3xl opacity-30"></div>
+
+      <div className="relative z-10 text-center px-6">
+        <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-8xl font-extrabold bg-gradient-to-r from-purple-400 to-pink-500 bg-clip-text text-transparent">
+          Presence
+        </h1>
+
+        <p className="xs:text-lg sm:text-xl md:text-2xl lg:text-3xl mt-6 max-w-lg mx-auto text-gray-300">
+          Welcome to Presence — a simple and smart way to manage attendance efficiently.
+        </p>
+
+        <div className="mt-8 flex gap-4 justify-center">
+          <Link to={'/login'} 
+                className="px-6 py-3 rounded-lg bg-purple-600 hover:bg-purple-700 transition cursor-pointer">
+                Login
+          </Link>
+          <Link to={'/register'} 
+                className="px-6 py-3 rounded-lg border border-purple-400 hover:bg-purple-500/20 transition cursor-pointer">
+                Register
+          </Link>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default Homepage
